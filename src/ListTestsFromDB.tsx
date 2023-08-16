8893894d208f7c8f4d44ecc8a83c610aa567f1a2
Anastasiia <anstasiakim2789@gmail.com>
2023-08-16 14:51:25 +0200

    in ListTestsFromDB created func for delete test firt we delete all questions of test, then all results of this test and lastly tets

--- src/ListTestsFromDB.tsx
+++ src/ListTestsFromDB.tsx
@@ -1,51 +1,82 @@
 import React, { useEffect, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { Avatar, Row, List, Button } from 'antd';
-import { useNavigate   } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { backendUrl } from './Global';
 
 interface ListTestsFromDBInterface {
-    name:string,
-    email:string,
-    id:number
+  name: string,
+  email: string,
+  id: number
 }
 const ListTestsFromDB = () => {
 
-  const navigate  = useNavigate();
+  const navigate = useNavigate();
 
-  const [listOfTests, setListOfTests]=useState<ListTestsFromDBInterface[]>([])
+  const [listOfTests, setListOfTests] = useState<ListTestsFromDBInterface[]>([])
 
   useEffect(() => {
     getAllTests()
-  },[]);
+  }, []);
 
-  const getAllTests=async()=>{
-    let response = await fetch(backendUrl + "/test/created?access_token="+localStorage.getItem("access_token") )
-    if(response.ok){
+  const getAllTests = async () => {
+    let response = await fetch(backendUrl + "/test/created?access_token=" + localStorage.getItem("access_token"))
+    if (response.ok) {
       let data = await response.json()
       setListOfTests(data)
     }
   }
+  const handleDelete = async (id: number) => {
 
-  return  (
-    <Row align="top" justify="center" style = {{ minHeight: '100vh', backgroundColor:"#EDEEF0", paddingTop:40}}>
+    let response = await fetch(backendUrl + '/createdTests?testId=' + id + "&access_token=" + localStorage.getItem("access_token"), {
+      method: 'DELETE'
+    })
+    if (response.ok) {
+      handleDeleteResultsOfTest(id)
+    }
 
-        <List
-            dataSource={listOfTests}
-            style={{padding:30, width:"50%",backgroundColor:"white"}}
+  };
+  const handleDeleteResultsOfTest = async (id: number) => {
 
-            renderItem={(item) => (
+    let response = await fetch(backendUrl + '/testresults?testId=' + id + "&access_token=" + localStorage.getItem("access_token"), {
+      method: 'DELETE'
+    })
+    if (response.ok) {
+      handleDeleteFromTestTest(id)
+    }
 
-            <List.Item key={item.email}>
-                <List.Item.Meta
-                title={item.name}
-                description={item.email}
-                />
-               <Button onClick={()=>{navigate("/testCreated/pass/"+item.email+"/"+item.name+"/"+item.id)}}>Aprobar el examen</Button>
-            </List.Item>
+  };
+  const handleDeleteFromTestTest =async (id:number) => {
 
-            )}
-        />
+    let response = await fetch(backendUrl + '/test?testId='+id+"&access_token="+localStorage.getItem("access_token"), {
+      method: 'DELETE'
+    })
+    if(response.ok){
+        getAllTests()
+    }
+    
+  };
+  return (
+    <Row align="top" justify="center" style={{ minHeight: '100vh', backgroundColor: "#EDEEF0", paddingTop: 40 }}>
+
+      <List
+        dataSource={listOfTests}
+        style={{ padding: 30, width: "50%", backgroundColor: "white" }}
+
+        renderItem={(item) => (
+
+          <List.Item key={item.email}>
+            <List.Item.Meta
+              title={item.name}
+              description={item.email}
+            />
+            <Button danger style={{ marginRight: 10 }} onClick={() => { handleDelete(item.id) }}>Delete</Button>
+            <Button onClick={() => { navigate("/testCreated/pass/" + item.email + "/" + item.name + "/" + item.id) }}>Aprobar el examen</Button>
+
+          </List.Item>
+
+        )}
+      />
 
     </Row>
   );
